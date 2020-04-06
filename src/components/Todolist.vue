1ba428416cfb19bc377726038cff5f6823cd83a2
liqiduan <lee.nju@live.com>
2020-04-06 06:47:52 -0700

    Todolist: Add support to get task in the database

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -2,21 +2,32 @@
   <v-container>
     <v-card v-for="task in tasks" :key='task._id'>
       <v-card-title>{{task.title}}</v-card-title>
-      <v-card-subtitle>{{task.desc}}</v-card-subtitle>
+      <v-card-subtitle>{{task.description}}</v-card-subtitle>
     </v-card>
   </v-container>
 </template>
 
 <script lang="ts">
+import {computed} from '@vue/composition-api'
+import {useFind} from 'feathers-vuex'
+
 export default {
   name: 'Todolist',
-  comptued: {
-    tasks: function () {
-      return [{
-        _id: 0,
-        title: "asdf",
-        desc: "adf"
-      }]
+
+  setup(props, context) {
+    const {Task} = context.root.$FeathersVuex.api
+    const params = computed(() => {
+      return {
+        query: {}
+      }
+    })
+
+    const data = useFind({
+      model: Task,
+      params: params,
+      })
+    return {
+      tasks: data.items
     }
   }
 }
